c90f13cbaa3b334f1d34b66abc1a623605c0e9f7
fzxiehui <xie18960920183@outlook.com>
2023-09-05 10:00:31 +0800

    feat: 完善user handler验证内容

--- {{cookiecutter.app_name}}/internal/handler/user.go
+++ {{cookiecutter.app_name}}/internal/handler/user.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/internal/pkg/request"
 	"github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/internal/service"
 	"github.com/gin-gonic/gin"
@@ -27,6 +29,14 @@ type userHandler struct {
 
 func (h *userHandler) Register(ctx *gin.Context) {
 	req := &request.RegisterRequest{}
-	h.userService.Register(ctx, req)
-
+	if err := ctx.BindJSON(req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := h.userService.Register(ctx, req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Register Success"})
 }
+
